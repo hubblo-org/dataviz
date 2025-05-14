f174a54fc7ecebe7a8ec50fd1c44327ce9119956
Benjamin Dromard <benjamin.dromard@gmail.com>
2025-05-14 15:18:36 +0200

    test: basic setup for snapshot testing

---
+++ test/plots/horizontalbarplot.ts
@@ -0,0 +1,18 @@
+import { horizontalBarPlot } from "../../src";
+
+const data = [
+  { type: "string", value: 1 },
+  { type: "number", value: 1 }
+];
+
+function renderHorizontalBarPlot() {
+  const plotId = "horizontal-bar-plot";
+  const mainDiv = document.getElementById("render-plot");
+  const plotDiv = document.createElement("div");
+  plotDiv.setAttribute("id", plotId);
+  mainDiv?.appendChild(plotDiv);
+
+  horizontalBarPlot(`#${plotId}`, data, 800, 600, "value", "type", true);
+}
+
+renderHorizontalBarPlot();
