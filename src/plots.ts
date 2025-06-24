eb9527375a82139804ecbc09908fdc1636c074c9
Benjamin Dromard <benjamin.dromard@gmail.com>
2025-06-24 11:01:04 +0200

    style: methods in alphabetical order

--- src/plots.ts
+++ src/plots.ts
@@ -20,6 +20,133 @@ import {
   text
 } from "@observablehq/plot";
 
+export function addLogo(nodeId: string, logo: string) {
+  // When a legend is created with the generated plot, a figure element is added to the selected div.
+  // The first child is then the div with the legend elements.
+  const logoDiv = select(nodeId).select("figure").select("div").append("div").attr("class", "logo");
+
+  logoDiv.append("img").attr("src", logo);
+  logoDiv.append("span").text("Hubblo").attr("class", "logo");
+}
+
+export function addSelect<Type>(
+  nodeId: string,
+  xLabel: string,
+  yLabel: string,
+  width: number,
+  data: Type
+): string {
+  const selectContainerId = `${nodeId}-select-container`;
+  const selectId = `${nodeId}-select`;
+  const divForSelect = document.querySelector(`#${selectContainerId}`);
+
+  if (!divForSelect) {
+    const parent = document.getElementById(nodeId).parentElement.nodeName;
+    const divForSelect = select(parent).append("div").attr("id", selectContainerId);
+    center(selectContainerId, width);
+
+    const isNotAnAxis = (value: string) => {
+      if (value === xLabel || value == yLabel) {
+        return false;
+      }
+      return true;
+    };
+
+    const selectStyle =
+      "background: 0 0; position: relative; border: 1px solid hsla(240, 6%, 87%, 1); border-radius: 4px; padding: 0.425em 1em 0.45em; min-height: 1.5rem; font: inherit;";
+    divForSelect
+      .append("label")
+      .attr("for", selectId)
+      .text("Select a property for value distribution: ");
+
+    divForSelect.append("select").attr("id", selectId).attr("style", selectStyle);
+    const options = Object.keys(data[0]).filter(isNotAnAxis);
+    options.forEach((option) =>
+      select(`#${selectId}`).append("option").attr("value", option).attr("id", option).text(option)
+    );
+
+  }
+    return selectId;
+}
+
+/** Renders a stacked area chart.
+ *
+ * @remarks
+ *
+ * One can use a stacked area chart when wanting to show the distribution of distinct
+ * groups among a whole. It is not so useful when one wants to show the individual evolution
+ * of each group {@link https://www.data-to-viz.com/graph/stackedarea.html}. A multi-line chart
+ * or a group of individual line chart / area chart can be an alternative in that case. Therefore,
+ * when not normalized, each data group will be its distinct area chart.
+ *
+ * @param nodeId - The DOM element where the plot will be rendered.
+ * @param data - The data structure to be rendered on the chart.
+ * @param width - The plot width, in pixels.
+ * @param height - The plot height, in pixels.
+ * @param xLabel - The data property to be rendered on the x axis.
+ * @param yLabel - The data property to be rendered on the y axis.
+ * @param zDimension - The third data property allowing the grouping of data on the chart.
+ * @param normalize - If true, data is normalized and the chart is stacked.
+ *
+ */
+export function areaChart<Type>(
+  nodeId: string,
+  data: Type,
+  width: number,
+  height: number,
+  xLabel: string,
+  yLabel: string,
+  zDimension: string,
+  normalize?: boolean
+) {
+  const plotOptions: PlotOptions = {
+    width: width,
+    height: height,
+    marginLeft: 0,
+    style: "overflow:visible;"
+  };
+
+  if (normalize) {
+    center(nodeId, width);
+    plotOptions.color = { legend: true };
+    plotOptions.round = true;
+    plotOptions.x = { label: null, insetLeft: 36, type: "time", ticks: "year" };
+    plotOptions.y = { label: "↑ Share (%)", percent: true };
+    plotOptions.marks = [
+      areaY(
+        data as Data,
+        stackY(
+          { offset: "normalize", order: zDimension, reverse: true },
+          { x: xLabel, y: yLabel, fill: zDimension, order: "group" }
+        )
+      ),
+      ruleY([0, 1])
+    ];
+  } else {
+    plotOptions.y = { grid: true, label: null };
+    plotOptions.marks = [
+      axisFy({ label: null, text: null }),
+      areaY(data as Data, {
+        x: (d) => new Date(d[xLabel]),
+        y: yLabel,
+        fy: zDimension,
+        fill: zDimension
+      }),
+      text(
+        data as Data,
+        selectFirst({ text: zDimension, fy: zDimension, frameAnchor: "top-left", dx: 6, dy: 6 })
+      ),
+      frame()
+    ];
+  }
+  const div = document.querySelector(`#${nodeId}`);
+  div?.firstChild?.remove();
+  if (div) {
+    const areaChart = plot(plotOptions);
+    div.append(areaChart);
+  }
+}
+
 function center(nodeId: string, width: number) {
   select(`#${nodeId}`).attr("style", `margin:auto; width: ${width}px`);
 }
@@ -82,53 +209,75 @@ export function horizontalBarPlot<Type>(
   }
 }
 
-export function addLogo(nodeId: string, logo: string) {
-  // When a legend is created with the generated plot, a figure element is added to the selected div.
-  // The first child is then the div with the legend elements.
-  const logoDiv = select(nodeId).select("figure").select("div").append("div").attr("class", "logo");
-
-  logoDiv.append("img").attr("src", logo);
-  logoDiv.append("span").text("Hubblo").attr("class", "logo");
-}
-
-export function addSelect<Type>(
+/** Renders a line or multi-line chart.
+ *
+ * @param nodeId - The DOM element where the plot will be rendered.
+ * @param data - The data structure to be rendered on the chart.
+ * @param width - The plot width, in pixels.
+ * @param height - The plot height, in pixels.
+ * @param xLabel - The data property to be rendered on the x axis.
+ * @param yLabel - The data property to be rendered on the y axis.
+ * @param zDimension - The third data property allowing the grouping of data and rendering multiple lines.
+ *
+ */
+export function lineChart<Type>(
   nodeId: string,
+  data: Type,
+  width: number,
+  height: number,
   xLabel: string,
   yLabel: string,
-  width: number,
-  data: Type
-): string {
-  const selectContainerId = `${nodeId}-select-container`;
-  const selectId = `${nodeId}-select`;
-  const divForSelect = document.querySelector(`#${selectContainerId}`);
+  zDimension?: string
+) {
+  let div = document.querySelector(nodeId);
+  div?.firstChild?.remove();
 
-  if (!divForSelect) {
-    const parent = document.getElementById(nodeId).parentElement.nodeName;
-    const divForSelect = select(parent).append("div").attr("id", selectContainerId);
-    center(selectContainerId, width);
+  const lineMarks = [
+    gridY({ strokeDasharray: "0.75,2", strokeOpacity: 1 }),
+    axisY({
+      tickSize: 0,
+      dx: 38,
+      dy: -6,
+      labelOffset: -36,
+      lineAnchor: "bottom"
+    }),
+    ruleY([0]),
+    line(data as Data, {
+      x: xLabel,
+      y: yLabel,
+      z: zDimension,
+      markerEnd: "dot",
+      stroke: zDimension
+    })
+  ];
 
-    const isNotAnAxis = (value: string) => {
-      if (value === xLabel || value == yLabel) {
-        return false;
-      }
-      return true;
-    };
+  const multiLineMarks = lineMarks.concat([
+    text(
+      data as Data,
+      selectLast({
+        x: xLabel,
+        y: yLabel,
+        z: zDimension,
+        text: zDimension,
+        textAnchor: "start",
+        dx: 3
+      })
+    )
+  ]);
 
-    const selectStyle =
-      "background: 0 0; position: relative; border: 1px solid hsla(240, 6%, 87%, 1); border-radius: 4px; padding: 0.425em 1em 0.45em; min-height: 1.5rem; font: inherit;";
-    divForSelect
-      .append("label")
-      .attr("for", selectId)
-      .text("Select a property for value distribution: ");
+  if (div) {
+    const lineChart = plot({
+      height: height,
+      marginLeft: 0,
+      round: true,
+      style: "overflow:visible",
+      width: width,
+      x: { label: null, insetLeft: 36, type: "time", ticks: "year" },
+      marks: zDimension ? multiLineMarks : lineMarks
+    });
 
-    divForSelect.append("select").attr("id", selectId).attr("style", selectStyle);
-    const options = Object.keys(data[0]).filter(isNotAnAxis);
-    options.forEach((option) =>
-      select(`#${selectId}`).append("option").attr("value", option).attr("id", option).text(option)
-    );
-
+    div.append(lineChart);
   }
-    return selectId;
 }
 
 /** Renders a bar plot, with each bar with stacked values.
@@ -225,154 +374,5 @@ export function stackedBarPlot<Type>(
       const fieldDomains = [...new Set(data.map((element: Type) => element[selectedProperty]))];
       stackedBarPlot(nodeId, data, width, height, fieldDomains, xLabel, yLabel, selectedProperty);
     });
-  }
-}
-
-/** Renders a line or multi-line chart.
- *
- * @param nodeId - The DOM element where the plot will be rendered.
- * @param data - The data structure to be rendered on the chart.
- * @param width - The plot width, in pixels.
- * @param height - The plot height, in pixels.
- * @param xLabel - The data property to be rendered on the x axis.
- * @param yLabel - The data property to be rendered on the y axis.
- * @param zDimension - The third data property allowing the grouping of data and rendering multiple lines.
- *
- */
-export function lineChart<Type>(
-  nodeId: string,
-  data: Type,
-  width: number,
-  height: number,
-  xLabel: string,
-  yLabel: string,
-  zDimension?: string
-) {
-  let div = document.querySelector(nodeId);
-  div?.firstChild?.remove();
-
-  const lineMarks = [
-    gridY({ strokeDasharray: "0.75,2", strokeOpacity: 1 }),
-    axisY({
-      tickSize: 0,
-      dx: 38,
-      dy: -6,
-      labelOffset: -36,
-      lineAnchor: "bottom"
-    }),
-    ruleY([0]),
-    line(data as Data, {
-      x: xLabel,
-      y: yLabel,
-      z: zDimension,
-      markerEnd: "dot",
-      stroke: zDimension
-    })
-  ];
-
-  const multiLineMarks = lineMarks.concat([
-    text(
-      data as Data,
-      selectLast({
-        x: xLabel,
-        y: yLabel,
-        z: zDimension,
-        text: zDimension,
-        textAnchor: "start",
-        dx: 3
-      })
-    )
-  ]);
-
-  if (div) {
-    const lineChart = plot({
-      height: height,
-      marginLeft: 0,
-      round: true,
-      style: "overflow:visible",
-      width: width,
-      x: { label: null, insetLeft: 36, type: "time", ticks: "year" },
-      marks: zDimension ? multiLineMarks : lineMarks
-    });
-
-    div.append(lineChart);
-  }
-}
-
-/** Renders a stacked area chart.
- *
- * @remarks
- *
- * One can use a stacked area chart when wanting to show the distribution of distinct
- * groups among a whole. It is not so useful when one wants to show the individual evolution
- * of each group {@link https://www.data-to-viz.com/graph/stackedarea.html}. A multi-line chart
- * or a group of individual line chart / area chart can be an alternative in that case. Therefore,
- * when not normalized, each data group will be its distinct area chart.
- *
- * @param nodeId - The DOM element where the plot will be rendered.
- * @param data - The data structure to be rendered on the chart.
- * @param width - The plot width, in pixels.
- * @param height - The plot height, in pixels.
- * @param xLabel - The data property to be rendered on the x axis.
- * @param yLabel - The data property to be rendered on the y axis.
- * @param zDimension - The third data property allowing the grouping of data on the chart.
- * @param normalize - If true, data is normalized and the chart is stacked.
- *
- */
-export function areaChart<Type>(
-  nodeId: string,
-  data: Type,
-  width: number,
-  height: number,
-  xLabel: string,
-  yLabel: string,
-  zDimension: string,
-  normalize?: boolean
-) {
-  const plotOptions: PlotOptions = {
-    width: width,
-    height: height,
-    marginLeft: 0,
-    style: "overflow:visible;"
-  };
-
-  if (normalize) {
-    center(nodeId, width);
-    plotOptions.color = { legend: true };
-    plotOptions.round = true;
-    plotOptions.x = { label: null, insetLeft: 36, type: "time", ticks: "year" };
-    plotOptions.y = { label: "↑ Share (%)", percent: true };
-    plotOptions.marks = [
-      areaY(
-        data as Data,
-        stackY(
-          { offset: "normalize", order: zDimension, reverse: true },
-          { x: xLabel, y: yLabel, fill: zDimension, order: "group" }
-        )
-      ),
-      ruleY([0, 1])
-    ];
-  } else {
-    plotOptions.y = { grid: true, label: null };
-    plotOptions.marks = [
-      axisFy({ label: null, text: null }),
-      areaY(data as Data, {
-        x: (d) => new Date(d[xLabel]),
-        y: yLabel,
-        fy: zDimension,
-        fill: zDimension
-      }),
-      text(
-        data as Data,
-        selectFirst({ text: zDimension, fy: zDimension, frameAnchor: "top-left", dx: 6, dy: 6 })
-      ),
-      frame()
-    ];
-  }
-  const div = document.querySelector(`#${nodeId}`);
-  div?.firstChild?.remove();
-  if (div) {
-    const areaChart = plot(plotOptions);
-    div.append(areaChart);
   }
 }
